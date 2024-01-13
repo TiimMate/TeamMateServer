3cd55e2c05ed358ba13c009ec91d4cb07bc64e9c
김민정 <mjeong@sookmyung.ac.kr>
2024-01-14 01:25:18 +0900

    Fix: models/index.js 파일에서 잘못된 export 수정

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,20 @@
-import express, { Request, Response, NextFunction } from "express";
 import dotenv from "dotenv";
+dotenv.config();
+
+import express, { Request, Response, NextFunction } from "express";
 import cors from "cors";
 
-import { sequelize } from "./models/index";
-import { response } from "../config/response";
-import { BaseError } from "../config/error";
-import { status } from "../config/response.status";
+import db from "./models/index";
+import { response } from "./config/response";
+import { BaseError } from "./config/error";
+import { status } from "./config/response.status";
 
-// import { usersRouter } from "./routes/users.route";
-
-dotenv.config();
+// import { authRouter } from "./routes/auth.route";
 
 const app = express();
 
 app.set("port", process.env.PORT || 3000);
-sequelize
+db.sequelize
     .sync({ force: false })
     .then(() => {
         console.log("데이터베이스 연결 성공");
@@ -27,17 +27,18 @@ app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// app.use("/users", usersRouter);
+// app.use("/auth", authRouter);
 
 app.use((req: Request, res: Response, next: NextFunction) => {
     const err = new BaseError(status.NOT_FOUND);
     next(err);
 });
 
-app.use((err: BaseError, req: Request, res: Response, next: NextFunction) => {
+app.use((err, req: Request, res: Response, next: NextFunction) => {
     res.locals.message = err.message;
     res.locals.err = process.env.NODE_ENV !== "production" ? err : {};
-    res.status(err.data.status || status.INTERNAL_SERVER_ERROR.status).send(response(err.data));
+    const error = err instanceof BaseError ? err : new BaseError(status.INTERNAL_SERVER_ERROR);
+    res.status(error.data.status).send(response(error.data));
 });
 
 app.listen(app.get("port"), () => {
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,32 +1,27 @@
 import { Sequelize, Dialect } from "sequelize";
-import { config } from "../../config/config";
 import fs from "fs";
 import path from "path";
+import { config as Config } from "../config/config";
 
-const developmentConfig = config.development;
-export const sequelize = new Sequelize(
-    developmentConfig.database,
-    developmentConfig.username,
-    developmentConfig.password,
-    {
-        host: developmentConfig.host,
-        dialect: developmentConfig.dialect as Dialect,
-    },
-);
+const env = process.env.NODE_ENV || "development";
+const config = Config[env];
+const db: any = {};
 
-interface Database {
-    sequelize: Sequelize;
-    [key: string]: any;
-}
-const db: Database = { sequelize };
+const sequelize = new Sequelize(config.database, config.username, config.password, {
+    host: config.host,
+    dialect: config.dialect as Dialect,
+});
+
+db.sequelize = sequelize;
 
 const basename = path.basename(__filename);
 fs.readdirSync(__dirname)
-    .filter((file) => file !== basename && file.slice(-3) === ".ts")
+    .filter((file) => {
+        return file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".ts";
+    })
     .forEach((file) => {
         const model = require(path.join(__dirname, file));
-        console.log(model.name);
-        db[model.default.name] = model;
+        db[model.name] = model;
         model.initiate(sequelize);
     });
 
@@ -35,3 +30,5 @@ Object.keys(db).forEach((modelName) => {
         db[modelName].associate(db);
     }
 });
+
+export default db;
