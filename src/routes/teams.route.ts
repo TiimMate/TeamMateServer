d97dacaf5fa9f7a0ed3c7d8f9f92f2a17a58cbfb
김민정 <mjeong@sookmyung.ac.kr>
2024-01-26 00:33:12 +0900

    Feat: 라우트 함수 추가 #13

--- src/routes/teams.route.ts
+++ src/routes/teams.route.ts
@@ -2,8 +2,8 @@ import express from "express";
 import asyncHandler from "express-async-handler";
 import { verifyUser } from "../middlewares/auth.middleware";
 import { validateBody } from "../middlewares/validate.middleware";
-import { createTeam } from "../schemas/team.schema";
-import { fetchTeamPreviewsByCategory, fetchTeamDetail, addTeam } from "../controllers/teams.controller";
+import { createTeam, updateTeam } from "../schemas/team.schema";
+import { fetchTeamPreviewsByCategory, fetchTeamDetail, addTeam, modifyTeam } from "../controllers/teams.controller";
 
 export const teamsRouter = express.Router();
 
@@ -13,6 +13,6 @@ teamsRouter.get("/", asyncHandler(fetchTeamPreviewsByCategory));
 
 teamsRouter.post("/", validateBody(createTeam), asyncHandler(addTeam));
 
-// teamsRouter.put("/:teamId", asyncHandler(modifyTeam));
+teamsRouter.put("/:teamId", validateBody(updateTeam), asyncHandler(modifyTeam));
 
 teamsRouter.get("/:teamId", asyncHandler(fetchTeamDetail));
