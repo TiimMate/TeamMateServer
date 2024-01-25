ae1e36ad2c13bafd23fd8bba8a1c4936ac424e6c
Doeun <doeun990802@gmail.com>
2024-01-25 16:46:05 +0900

    Fix: 컨트롤러 함수 수정 #10

--- src/controllers/games.controller.ts
+++ src/controllers/games.controller.ts
@@ -1,7 +1,7 @@
 import { Response } from "express";
 import { response } from "../config/response";
 import { status } from "../config/response.status";
-import { readGamesByDate, readGamesByGender, readGamesByLevel, readGamesByRegion } from "../services/teams.service";
+import { readGamesByDate, readGamesByGender, readGamesByLevel, readGamesByRegion } from "../services/games.service";
 
 export const fetchGamesByDate = async (req, res, next) => {
     res.send(response(status.SUCCESS, await readGamesByDate(req.user.id, req.params)));
