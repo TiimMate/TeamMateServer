7459e097e6cd950ac3cecf7e6bc2f9bc4fcfc6c4
김민정 <mjeong@sookmyung.ac.kr>
2024-01-26 00:33:29 +0900

    Feat: 컨트롤러 함수 추가 #13

--- src/controllers/teams.controller.ts
+++ src/controllers/teams.controller.ts
@@ -1,7 +1,7 @@
 import { Response } from "express";
 import { response } from "../config/response";
 import { status } from "../config/response.status";
-import { readTeamPreviewsByCategory, readTeamDetail, createTeam } from "../services/teams.service";
+import { readTeamPreviewsByCategory, readTeamDetail, createTeam, updateTeam } from "../services/teams.service";
 
 export const fetchTeamPreviewsByCategory = async (req, res: Response, next) => {
     res.send(response(status.SUCCESS, await readTeamPreviewsByCategory(req.user.id, req.query)));
@@ -11,9 +11,9 @@ export const addTeam = async (req, res: Response, next) => {
     res.send(response(status.SUCCESS, await createTeam(req.user.id, req.body)));
 };
 
-// export const modifyTeam = async (req, res: Response, next) => {
-//     res.send(response(status.SUCCESS, await updateTeam(req.body)));
-// };
+export const modifyTeam = async (req, res: Response, next) => {
+    res.send(response(status.SUCCESS, await updateTeam(req.user.id, req.params, req.body)));
+};
 
 export const fetchTeamDetail = async (req, res: Response, next) => {
     res.send(response(status.SUCCESS, await readTeamDetail(req.user.id, req.params)));
