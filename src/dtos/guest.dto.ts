800431718f3fb3c0170e30033a83f3869b46916a
김민정 <mjeong@sookmyung.ac.kr>
2024-01-26 00:49:34 +0900

    Feat: 성별, 연령대, 레벨을 숫자에서 문자열로 변환하도록 readGuestingResponseDTO 수정 #11

--- src/dtos/guest.dto.ts
+++ src/dtos/guest.dto.ts
@@ -1,13 +1,16 @@
+import { getAgeGroupById } from "../constants/age-group.constant";
+import { getGenderById } from "../constants/gender.constant";
+import { getLevelById } from "../constants/level.constant";
+
 export const readGuestingResponseDTO = (guestings) => {
-    console.log(guestings);
     return guestings.map((guesting) => ({
         gameTime: guesting.gameTime,
         teamName: guesting["Team.name"],
         teamRegion: guesting["Team.region"],
-        teamGender: guesting["Team.gender"], //getGenderById(guesting["Team.gender"]),
+        teamGender: getGenderById(guesting["Team.gender"]),
         memberCount: guesting.memberCount,
-        teamAgeGroup: guesting["Team.ageGroup"], //getAgeGroupById(guesting["Team.ageGroup"]),
-        teamSkillLevel: guesting["Team.skillLevel"], //getLevelById(guesting["Team.skillLevel"])
+        teamAgeGroup: getAgeGroupById(guesting["Team.ageGroup"]),
+        teamSkillLevel: getLevelById(guesting["Team.skillLevel"]),
         recruitCount: guesting.recruitCount,
     }));
 };
