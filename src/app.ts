b56875448594c595ccad96e369acc470eb96110d
Doeun <doeun990802@gmail.com>
2024-01-25 13:52:26 +0900

    Feat: 라우트 추가 #10

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,7 @@ import { status } from "./config/response.status";
 import { authRouter } from "./routes/auth.route";
 import { teamsRouter } from "./routes/teams.route";
 import { membersRouter } from "./routes/members.route";
+import { gamesRouter } from "./routes/games.route";
 
 const app = express();
 
@@ -35,6 +36,7 @@ app.use(passport.initialize());
 app.use("/auth", authRouter);
 app.use("/teams", teamsRouter);
 app.use("/members", membersRouter);
+app.use("/games", gamesRouter);
 
 app.use((req: Request, res: Response, next: NextFunction) => {
     const err = new BaseError(status.NOT_FOUND);
