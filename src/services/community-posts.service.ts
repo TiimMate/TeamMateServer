a9dd2491be979c376e63b2ac67bac5d640693b3d
김민정 <mjeong@sookmyung.ac.kr>
2024-01-26 12:33:59 +0900

    Feat: 서비스 함수 추가 #19

--- src/services/community-posts.service.ts
+++ src/services/community-posts.service.ts
@@ -1,7 +1,13 @@
+import { insertOrDeleteBookmark } from "../daos/community-bookmark.dao";
 import { findCommunityPost } from "../daos/community-post.dao";
 import { readCommunityPostsResponseDTO } from "../dtos/community-posts.dto";
 
 export const readCommunityPosts = async (userId: number | undefined, query) => {
     const response = await findCommunityPost(userId, query.cursorId);
     return readCommunityPostsResponseDTO(response);
+};
+
+export const createOrDeleteBookmark = async (userId, params) => {
+    await insertOrDeleteBookmark(userId, params.postId);
+    return;
 };
