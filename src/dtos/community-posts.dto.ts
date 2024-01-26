de83a7a566f2a0e64977760bdb8cc208f2fb2331
김민정 <mjeong@sookmyung.ac.kr>
2024-01-26 12:09:35 +0900

    Feat: DTO 함수 추가 #17

--- src/dtos/community-posts.dto.ts
+++ src/dtos/community-posts.dto.ts
@@ -1,3 +1,11 @@
-export const readCommunityPostsResponseDTO = (posts) => {
-    return;
+export const readCommunityPostsResponseDTO = (response) => {
+    return {
+        posts: response.posts.map((post) => ({
+            id: post.id,
+            isBookmarked: Boolean(post["CommunityBookmarks.id"]),
+            title: post.title,
+            createdAt: post.createdAt,
+        })),
+        hasNext: response.hasNext,
+    };
 };
