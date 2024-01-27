4a3ef3510ce010373d5033203b800a0282cc3913
김민정 <mjeong@sookmyung.ac.kr>
2024-01-27 20:20:39 +0900

    Feat: 컨트롤러 함수 추가 #32

--- src/controllers/posts.controller.ts
+++ src/controllers/posts.controller.ts
@@ -1,7 +1,7 @@
 import { Response } from "express";
 import { response } from "../config/response";
 import { status } from "../config/response.status";
-import { readCommunityPosts, readPostsByAuthor } from "../services/posts.service";
+import { readBookmarkedPosts, readCommunityPosts, readPostsByAuthor } from "../services/posts.service";
 
 export const fetchCommunityPosts = async (req, res: Response, next) => {
     res.send(response(status.SUCCESS, await readCommunityPosts(req.user?.id, req.query)));
@@ -9,4 +9,8 @@ export const fetchCommunityPosts = async (req, res: Response, next) => {
 
 export const fetchMyPosts = async (req, res: Response, next) => {
     res.send(response(status.SUCCESS, await readPostsByAuthor(req.user.id, req.query)));
+};
+
+export const fetchBookmarkedPosts = async (req, res: Response, next) => {
+    res.send(response(status.SUCCESS, await readBookmarkedPosts(req.user.id, req.query)));
 };
