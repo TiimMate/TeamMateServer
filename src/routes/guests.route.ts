99d338fe3985a92980d5f70f92ca2e74f93231e2
ByeonYujin124 <byj7176@gmail.com>
2024-01-28 16:38:27 +0900

    FEAT) 게스트 모집글 작성 API 수정 #38

--- src/routes/guests.route.ts
+++ src/routes/guests.route.ts
@@ -6,13 +6,14 @@ import {
     GuestingPreviewByGender,
     GuestingPreviewByRegion,
     DetailedGuestingPreview,
+    addGuesting,
 } from "../controllers/guest.controller";
 import { createGuesting } from "../schemas/guest.schema";
 import { validateBody } from "../middlewares/validate.middleware";
 
 export const guestsRouter = express.Router({ mergeParams: true });
 
-// guestsRouter.post("/", validateBody(createGuesting), asyncHandler(addGuesting));
+guestsRouter.post("/", validateBody(createGuesting), asyncHandler(addGuesting));
 
 guestsRouter.get("/", asyncHandler(GuestingPreview));
 
