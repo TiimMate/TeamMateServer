75ec89d84bf399828f7e013aa7c8fc5a67df8585
Doeun <doeun990802@gmail.com>
2024-01-30 01:11:21 +0900

    Feat: games 컨트롤러 함수 추가 #46

--- src/controllers/games.controller.ts
+++ src/controllers/games.controller.ts
@@ -2,6 +2,7 @@ import { Response } from "express";
 import { response } from "../config/response";
 import { status } from "../config/response.status";
 import { readGamesByDate, readGamesByGender, readGamesByLevel, readGamesByRegion } from "../services/games.service";
+import { readTeamAvailPreviewById } from "../services/teams.service";
 
 export const fetchGamesByDate = async (req, res, next) => {
     res.send(response(status.SUCCESS, await readGamesByDate(req.query)));
@@ -17,4 +18,9 @@ export const fetchGamesByLevel = async (req, res, next) => {
 
 export const fetchGamesByRegion = async (req, res, next) => {
     res.send(response(status.SUCCESS, await readGamesByRegion(req.query)));
+};
+
+export const fetchTeamsAvailById = async (req, res, next) => {
+    // res.send(response(status.SUCCESS, await readTeamAvailPreviewById(req.user.id, req.query)));
+    res.send(response(status.SUCCESS, await readTeamAvailPreviewById(1, req.query))); // test
 };
