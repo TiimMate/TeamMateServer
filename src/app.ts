fc7c8186d16e800c24a2fc206cd454a9269e69c8
김민정 <mjeong@sookmyung.ac.kr>
2024-01-30 16:55:22 +0900

    Feat: 라우트 추가 #49

--- src/app.ts
+++ src/app.ts
@@ -15,6 +15,7 @@ import { membersRouter } from "./routes/members.route";
 import { gamesRouter } from "./routes/games.route";
 import { guestsRouter } from "./routes/guests.route";
 import { postsRouter } from "./routes/posts.route";
+import { usersRouter } from "./routes/users.route";
 
 const app = express();
 
@@ -38,6 +39,7 @@ app.use("/members", membersRouter);
 app.use("/games", gamesRouter);
 app.use("/guests", guestsRouter);
 app.use("/posts", postsRouter);
+app.use("/users", usersRouter);
 
 app.use((req: Request, res: Response, next: NextFunction) => {
     const err = new BaseError(status.NOT_FOUND);
