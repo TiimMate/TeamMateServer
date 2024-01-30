2bd5be82fd2afa3e063e65fd5d5a3f986e06c2a6
김민정 <mjeong@sookmyung.ac.kr>
2024-01-30 17:00:19 +0900

    Feat: update user profile schema 생성 #49

---
+++ src/schemas/fields.ts
@@ -0,0 +1,46 @@
+import { z } from "zod";
+import { Gender } from "../types/gender.enum";
+import { Category } from "../types/category.enum";
+import { AgeGroup } from "../types/age-group.enum";
+
+export const nicknameField = { nickname: z.string().max(10) };
+
+export const descriptionField = { description: z.optional(z.string().max(400)) };
+
+export const genderFieldInUser = { gender: z.optional(z.enum([Gender.Female, Gender.Male])) };
+
+export const genderFieldInTeam = { gender: z.enum([Gender.Female, Gender.Male, Gender.Mixed]) };
+
+const ageGroup = z.enum([
+    AgeGroup.Teenagers,
+    AgeGroup.Twenties,
+    AgeGroup.Thirties,
+    AgeGroup.Forties,
+    AgeGroup.FiftiesAndAbove,
+]);
+
+export const ageGroupFieldInUser = { ageGroup: z.optional(ageGroup) };
+
+export const ageGroupFieldInTeam = { ageGroup: ageGroup };
+
+export const heightField = { height: z.optional(z.number().int()) };
+
+export const regionFieldInProfile = { region: z.optional(z.string()) };
+
+export const regionFieldInTeam = { region: z.string() };
+
+export const positionField = { position: z.optional(z.string()) }; //TODO: 추후 enum으로 변경
+
+export const categoryField = {
+    category: z.enum([
+        Category.Basketball,
+        Category.Baseball,
+        Category.Tennis,
+        Category.Soccer,
+        Category.Futsal,
+        Category.Volleyball,
+        Category.Bowling,
+        Category.Badminton,
+        Category.TableTennis,
+    ]),
+};
