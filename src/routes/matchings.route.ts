f04246862907492e11ba5460bcacc7211e25350e
ByeonYujin124 <byj7176@gmail.com>
2024-02-02 14:10:03 +0900

    Fix) varifyUser 추가 #41

--- src/routes/matchings.route.ts
+++ src/routes/matchings.route.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import asyncHandler from "express-async-handler";
+import { verifyUser } from "../middlewares/auth.middleware";
 import {
     ApplyGuestingUserPreview,
     matchingGuestingPreview,
@@ -9,10 +10,12 @@ import {
 
 export const matchingsRouter = express.Router({ mergeParams: true });
 
+matchingsRouter.use(verifyUser);
+
 matchingsRouter.get("/guesting", asyncHandler(matchingGuestingPreview));
 
 matchingsRouter.get("/hosting", asyncHandler(matchingHostingPreview));
 
-matchingsRouter.get("/hosting/appliedGuests/:guestingId", asyncHandler(ApplyGuestingUserPreview));
+matchingsRouter.get("/hosting/:guestingId", asyncHandler(ApplyGuestingUserPreview));
 
-matchingsRouter.put("/confirmGuestingUser/:guestUserId", asyncHandler(modifyGuestStatus));
+matchingsRouter.put("/confirmGuest/:guestUserId", asyncHandler(modifyGuestStatus));
