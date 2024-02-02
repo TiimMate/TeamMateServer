99426f575d3dfa9b514c17f1215f193f036f4641
Doeun <doeun990802@gmail.com>
2024-02-03 00:41:37 +0900

    Feat: 컨트롤러 함수 추가 #77

--- src/controllers/matchings.controller.ts
+++ src/controllers/matchings.controller.ts
@@ -1,6 +1,7 @@
 import { response } from "../config/response";
 import { status } from "../config/response.status";
 import { readMatchingGuesting, readMatchingHosting, readHostingApplicantsList } from "../services/matchings.service";
+import { addOpposingTeam } from "../services/teams.service";
 
 export const matchingGuestingPreview = async (req, res, next) => {
     return res.send(response(status.SUCCESS, await readMatchingGuesting(2, req.query)));
@@ -15,4 +16,9 @@ export const matchingHostingPreview = async (req, res, next) => {
 export const fetchHostingApplicantsTeamList = async (req, res, next) => {
     // return res.send(response(status.SUCCESS, await readHostingApplicantsList(req.user.id, req.query)));
     return res.send(response(status.SUCCESS, await readHostingApplicantsList(1, req.params)));
+};
+
+export const gameApplicationApproval = async (req, res, next) => {
+    return res.send(response(status.SUCCESS, await readHostingApplicantsList(req.user.id, req.query)));
+    // return res.send(response(status.SUCCESS, await addOpposingTeam(1, req.params, req.body))); // for testing
 };
