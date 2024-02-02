0dba240706014daec5bfb93f1bba36927113abde
Doeun <doeun990802@gmail.com>
2024-02-03 00:41:27 +0900

    Feat: 라우트 추가 #77

--- src/routes/matchings.route.ts
+++ src/routes/matchings.route.ts
@@ -4,6 +4,7 @@ import {
     matchingGuestingPreview,
     matchingHostingPreview,
     fetchHostingApplicantsTeamList,
+    gameApplicationApproval,
 } from "../controllers/matchings.controller";
 
 export const matchingsRouter = express.Router({ mergeParams: true });
@@ -14,3 +15,6 @@ matchingsRouter.get("/hosting", asyncHandler(matchingHostingPreview));
 
 // 호스팅 내역 > 신청팀 목록 조회
 matchingsRouter.get("/hosting/:gameId", asyncHandler(fetchHostingApplicantsTeamList));
+
+// 호스팅 내역 > 신청 승인
+matchingsRouter.post("/hosting/:gameId", asyncHandler(gameApplicationApproval));
