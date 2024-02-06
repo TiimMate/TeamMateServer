52463698ef2eb57f1aaaa2664f39ad9cb611687b
김민정 <mjeong@sookmyung.ac.kr>
2024-02-06 20:23:14 +0900

    Refactor: Record를 이용한 Statuses로 변경

--- src/constants/status.constant.ts
+++ src/constants/status.constant.ts
@@ -1,14 +1,10 @@
-type Status = {
-    name: string;
-};
-
-const Statuss: { [key: number]: Status } = {
-    0: { name: "모집 중" },
-    1: { name: "모집 완료" },
+const Statuses: Record<number, string> = {
+    0: "모집 중",
+    1: "모집 완료",
 };
 
 export const defaultStatus = 0;
 
-export const getStatus = (id: number): string | undefined => {
-    return Statuss[id].name;
+export const getStatus = (key: number): string | undefined => {
+    return Statuses[key];
 };
