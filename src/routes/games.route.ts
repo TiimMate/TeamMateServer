da3837c398eb866c3399478ddcb3554261b16066
Doeun <doeun990802@gmail.com>
2024-02-06 15:04:06 +0900

    Feat: games.route 파일에 verifyUser 미들웨어 추가

--- src/routes/games.route.ts
+++ src/routes/games.route.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import asyncHandler from "express-async-handler";
+import { verifyUser } from "../middlewares/auth.middleware";
+import { categoryParam, dateParam, genderParam, levelParam, regionParam } from "../schemas/fields";
 import {
     fetchGamesByDate,
     fetchGamesByGender,
@@ -20,19 +22,39 @@ export const gamesRouter = express.Router();
 gamesRouter.get("/", asyncHandler(fetchGamesByDate));
 
 // 연습경기 모집글 필터링 - 성별/레벨/지역
-gamesRouter.get("/by-gender", asyncHandler(fetchGamesByGender));
-gamesRouter.get("/by-level", asyncHandler(fetchGamesByLevel));
-gamesRouter.get("/by-region", asyncHandler(fetchGamesByRegion));
+gamesRouter.get(
+    "/by-gender",
+    validate(categoryParam),
+    validate(dateParam),
+    validate(genderParam),
+    asyncHandler(fetchGamesByGender),
+);
+
+gamesRouter.get(
+    "/by-level",
+    validate(categoryParam),
+    validate(dateParam),
+    validate(levelParam),
+    asyncHandler(fetchGamesByLevel),
+);
+
+gamesRouter.get(
+    "/by-region",
+    validate(categoryParam),
+    validate(dateParam),
+    validate(regionParam),
+    asyncHandler(fetchGamesByRegion),
+);
 
 // 연습경기 신청 가능 팀 목록 조회
 gamesRouter.get("/apply-avail", asyncHandler(fetchTeamsAvailById));
 
 // 연습경기 신청
-gamesRouter.post("/:gameId/application", asyncHandler(applyGame));
+gamesRouter.post("/:gameId/application", verifyUser, asyncHandler(applyGame));
 
 // 연습경기 모집글 상세 조회
-gamesRouter.get("/:gameId", asyncHandler(fetchGameDetail));
+gamesRouter.get("/:gameId", verifyUser, asyncHandler(fetchGameDetail));
 
 // 연습경기 모집글 작성/수정
-gamesRouter.post("/", validate(createGameSchema), asyncHandler(addGame));
-gamesRouter.put("/:gameId", validate(updateGameSchema), asyncHandler(modifyGame));
+gamesRouter.post("/", verifyUser, validate(createGameSchema), asyncHandler(addGame));
+gamesRouter.put("/:gameId", verifyUser, validate(updateGameSchema), asyncHandler(modifyGame));
