7884d15d7776884a17cc2977e73d17b94644fb9a
ByeonYujin124 <byj7176@gmail.com>
2024-02-06 19:29:47 +0900

    Fix) guest schema 추가 및 validate 설정 변경 #97

--- src/schemas/guest.schema.ts
+++ src/schemas/guest.schema.ts
@@ -1,5 +1,15 @@
 import { TypeOf, object } from "zod";
-import { teamIdField, gameTimeField, descriptionField, recruitCountField } from "./fields";
+import {
+    teamIdField,
+    gameTimeField,
+    descriptionField,
+    recruitCountField,
+    categoryField,
+    dateField,
+    levelField,
+    regionFieldInTeam,
+    genderFieldInTeam,
+} from "./fields";
 
 const body = {
     ...teamIdField,
@@ -16,12 +26,56 @@ const updateGuestBody = object({
     ...body,
 });
 
+const createGuestQuery = object({
+    ...categoryField,
+});
+
 export const createGuestingSchema = object({
     body: createGuestBody,
+    query: createGuestQuery,
 });
 
 export const updateGuestingSchema = object({
     body: updateGuestBody,
+});
+
+const readGuest = {
+    ...categoryField,
+    ...dateField,
+};
+
+export const readGuestSchema = object({
+    query: object({
+        ...readGuest,
+        ...dateField,
+    }),
+});
+
+export const readGuestFilterGenderSchema = object({
+    query: object({
+        ...readGuest,
+        ...genderFieldInTeam,
+    }),
+});
+
+export const readGuestFilterLevelSchema = object({
+    query: object({
+        ...readGuest,
+        ...levelField,
+    }),
+});
+
+export const readGuestFilterRegionSchema = object({
+    query: object({
+        ...readGuest,
+        ...regionFieldInTeam,
+    }),
+});
+
+export const addGuestUserSchema = object({
+    query: object({
+        ...categoryField,
+    }),
 });
 
 export type CreateGuestingBody = TypeOf<typeof createGuestBody>;
