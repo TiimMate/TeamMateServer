61631003811208a32b29031659412d9ebf76421c
ByeonYujin124 <byj7176@gmail.com>
2024-02-14 22:45:19 +0900

    Refactor: getLevelById 함수 사용하는 부분 삭제 #132

--- src/dtos/users.dto.ts
+++ src/dtos/users.dto.ts
@@ -1,6 +1,5 @@
 import { getAgeGroup } from "../constants/age-group.constant";
 import { getGender } from "../constants/gender.constant";
-import { getLevelById } from "../constants/level.constant";
 import { AgeGroup } from "../types/age-group.enum";
 import { Gender } from "../types/gender.enum";
 
@@ -20,8 +19,8 @@ interface ReadUserProfile {
 export const readUserProfileResponseDTO = (profile: ReadUserProfile) => {
     return {
         nickname: profile.nickname,
-        skillLevel: !profile["Profiles.skillLevel"] ? null : getLevelById(profile["Profiles.skillLevel"]),
-        mannerLevel: !profile["Profiles.mannerLevel"] ? null : getLevelById(profile["Profiles.mannerLevel"]),
+        skillLevel: !profile["Profiles.skillLevel"] ? null : profile["Profiles.skillLevel"],
+        mannerLevel: !profile["Profiles.mannerLevel"] ? null : profile["Profiles.mannerLevel"],
         gender: !profile.gender ? null : getGender(profile.gender),
         ageGroup: !profile.ageGroup ? null : getAgeGroup(profile.ageGroup),
         region: profile["Profiles.region"],
